7734d8d40624ee55ca087a6d85d56a536c35fc59
tegmenbugo <163106105+tegmenbugo@users.noreply.github.com>
2026-01-05 20:22:20 +0300

    Vizelerden sonraki yaptıklarımız eklendi (hata düzeltmesi).

--- laravel/app/Http/Controllers/CustomerController.php
+++ laravel/app/Http/Controllers/CustomerController.php
@@ -3,71 +3,71 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Customer; // 1. EKSİK BUYDU: Modeli içeri çağırdık.
 
 class CustomerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $customers = Customer::all();
-        return view('customer.index',compact('customers'));
+        
+        return view('customers.index', compact('customers')); 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('customers.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        $customer = Customer::create([
-            'name'=>request->post('name'),
-            'surname'=>request->post('surname'),
-            'gender'=>request->post('gender'),
-            'birthyear'=>request->post('birthyear')
-        ]);
-        return redirect()->route('customers.index');
         
+        Customer::create([
+            'name'      => $request->name,
+            'surname'   => $request->surname,
+            'gender'    => $request->gender,
+            'birthyear' => $request->birthyear
+        ]);
+        
+        return redirect()->route('customers.index');
+    } 
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        
+        $customer = Customer::find($id); 
+        return view('customers.show', compact('customer'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
-        return view('customers.edit',compact('customer'));
+        
+        $customer = Customer::find($id);
+        return view('customers.edit', compact('customer'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        $customer->update($request->all());
+        
+        $customer = Customer::find($id);
+        
+        
+        $customer->update([
+            'name'      => $request->name,
+            'surname'   => $request->surname,
+            'gender'    => $request->gender,
+            'birthyear' => $request->birthyear
+        ]);
+
         return redirect()->route('customers.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
+        
+        $customer = Customer::find($id);
         $customer->delete();
+        
         return redirect()->route('customers.index');
     }
 }
